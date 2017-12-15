c347d8c90536f07e8312a53c2f70fc7057d492d9
Aris Tomaras <arisgold29@gmail.com>
2017-12-16 01:08:56 +0200

    simple send-receive client function added in utils

---
+++ minicash/utils/client.py
@@ -0,0 +1,39 @@
+import socket
+
+def simpleSend(data, remoteaddrs, port, waitResponse=False, timeout=None):
+    for addr in remoteaddrs:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((addr, port))
+            s.sendall(data.encode('utf-8'))
+            if waitResponse == True:
+                s.settimeout(timeout)
+                response = str(s.recv(1024), 'utf-8')
+                print('Response from {}:{} => {}'.format(addr, str(port), response))
+        except socket.timeout:
+            print('Timeout error waiting for response from {}'.format(addr))
+            continue
+        except OSError as e:
+            print('OSError: {}'.format(e))
+            continue
+        finally:
+            s.close()
+
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data', type=str, help='Text to send')
+    parser.add_argument('remoteaddrs', type=str, help='Comma separated ips')
+    parser.add_argument('port', type=int, help='Port to connect')
+    parser.add_argument('--wait', dest='wait', action='store_true')
+    parser.add_argument('--no-wait', dest='wait', action='store_false')
+    parser.set_defaults(wait=True)
+    parser.add_argument('timeout', type=float, default=None, help='Timeout if waiting for response')
+    args = parser.parse_args()
+
+    remotes = list(args.remoteaddrs.split(','))
+    simpleSend(args.data, remotes, args.port, args.wait, args.timeout)
+
