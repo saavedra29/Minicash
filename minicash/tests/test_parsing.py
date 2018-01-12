eb6f98739cc86965c773d883d9736fe2066bc675
Aris Tomaras <arisgold29@gmail.com>
2018-01-12 19:35:08 +0200

    parser for ledger and ledger response format added - tests added

---
+++ minicash/tests/test_parsing.py
@@ -0,0 +1,71 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from utils.parsers import isValidLedger
+from utils.parsers import isValidLedgerResponseFormat
+
+class TestValidLedger(unittest.TestCase):
+    
+    def testValidLedger(self):
+        validLedgers = [
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':3242434120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':0, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {},
+            {'8D972AA78B46CBF7_124625':98},
+            {'8D972AA78B46CBF7_124625':0}
+                ]
+
+        invalidLedgers = [
+            {'CC0504FD53A5A5E7_279327':120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793472':120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':-120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':None, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':True, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {34335234:120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, True:23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120.454, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':103.0, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':'jdfkjldjf', '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E6_2793272':120, 'C4ED6700DFB2A1DF_2514606':23, '0FC085CAAB8059F0_802809':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':0.3432, '1BE0B920ABB6D44A_3208670':47, '8D972AA78B46CBF7_124625':98},
+            {'CC0504FD53A5A5E7_2793272':120, 'C4ED6700DFB2A1DF_2514606':23, '1BE0B900ABB6D44A_320860':47, '8D972AA78B46CBF7_124625':-98},
+            'kdfjldkjfkl', 2342434, True
+                ]
+
+            
+        for v in validLedgers:
+            self.assertTrue(isValidLedger(v))
+        for v in invalidLedgers:
+            self.assertFalse(isValidLedger(v))
+
+    def testValidResponseFormat(self):
+        validResponses = [
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'34224324', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{}, 'Type': 'RESP_LEDGER'}
+        ]
+
+        invalidResponses = [
+            {'Leder':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Sigatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B4ZCBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER', 'extra_thing':'dfjdlfkj'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Hype': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':2344}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', 49:'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'klfjldj', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': {}},
+            {},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':3234, 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESP_LEDGER'},
+            {'Ledger':{}, 'Signatures':{'8D972AA78B46CBF7':'dkfjldjk', 'CC0504FD53A5A5E7':'', '3EE3FD7A50CBD975':'kdjfljdklj'}, 'Type': 'RESPLEDGER'}
+        ]
+
+        for v in validResponses:
+            self.assertTrue(isValidLedgerResponseFormat(v))
+        for v in invalidResponses:
+            self.assertFalse(isValidLedgerResponseFormat(v))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ minicash/utils/parsers.py
@@ -0,0 +1,67 @@
+import re
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from utils.checksum import isValidProof
+
+# Checks for the ledger's format since it's of dict type
+def isValidLedger(ledger):
+    if type(ledger) is not dict:
+        return False
+    # check if keys are strings and values are integers
+    for key in ledger.keys():
+        if type(key) is not str:
+            return False
+    for balance in ledger.values():
+        if type(balance) is not int:
+            return False
+    for key, value in ledger.items():
+        # Check if key first 16 characters are valid fingerprint
+        fprint = key[:16]
+        res = re.match('^[a-fA-F0-9]{16}$', fprint)
+        if res == None:
+            return False
+        # Check if 17th character is _
+        if key[16] != '_':
+            return False
+        # Check if [17:] is representing integer
+        proof = key[17:]
+        try:
+            proofint = int(proof)
+        except ValueError:
+            return False
+        # Check proof with proof of work checker for the key validity
+        if not isValidProof(fprint, proof):
+            return False
+        # Check if balance is 0 or above
+        if value < 0:
+            return False
+    return True
+
+
+# Checks if he ledger response is formated correctly. This is a dict
+def isValidLedgerResponseFormat(res):
+    if type(res) is not dict:
+        return False 
+    if not ('Ledger' in res and 'Signatures' in res and 'Type' in res):
+        return False 
+    if res['Type'] != 'RESP_LEDGER':
+        return False
+    if not isValidLedger(res['Ledger']):
+        return False
+    if len(res) != 3:
+        return False
+    # Check if signatures value is dictionary
+    signatures = res['Signatures']
+    if type(signatures) is not dict:
+        return False
+    for fprint, sig in signatures.items():
+        if type(fprint) is not str or type(sig) is not str:
+            return False
+        res = re.match('^[a-fA-F0-9]{16}$', fprint)
+        if res == None:
+            return False
+        if type(sig) is not str:
+            return False
+    return True
+
