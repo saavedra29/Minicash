b3baec3934308f6f55e015f8b696b6c95949d374
Aris Tomaras <arisgold29@gmail.com>
2018-02-11 15:57:15 +0200

    v0.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Minicash',
-    version='0.4.0',
+    version='0.4.1',
     description='A cryptocurrency without blockchain',
     url='http://github.com/saavedra29/Minicash',
     author='Aristides Tomaras',
